7dcc23668799321eb252f9a458bddd2d0196286f
Ahmed Softev <ahmed_v@abv.bg>
2021-09-07 20:15:16 +0300

    js

--- Syntax, Functions and Statements - Exercise/06. Cooking by Numbers.js
+++ Syntax, Functions and Statements - Exercise/06. Cooking by Numbers.js
@@ -7,27 +7,27 @@ function solve(num, com1, com2, com3, com4, com5) {
         switch (arr[i]) {
             case 'chop':
                 number /= 2;
-                console.log(number);
+                
                 break;
             case 'dice':
                 number = Math.sqrt(number);
-                console.log(number);
+               
                 break;
             case 'spice':
                 number++;
-                console.log(number);
+                
                 break;
             case 'bake':
                 number *= 3;
-                console.log(number);
+                
                 break;
             case 'fillet':
                 number *= 0.8;
-                console.log(number);
+               
                 break;
         
         }
-        
+        console.log(number);
     }
     
 }
