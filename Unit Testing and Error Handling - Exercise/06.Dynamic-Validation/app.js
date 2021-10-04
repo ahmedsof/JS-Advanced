6556fa04f565f50e2c570f783d5ae470d9647be8
Ahmed Softev <ahmed_v@abv.bg>
2021-10-04 19:59:00 +0300

    JS

--- Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+  const input = document.getElementById("email");
+  const pattern = /[a-z]+\@[a-z]+\.[a-z]{2,3}/gm
+      // /^\w+@\w+.\w+$/gm;
+  input.addEventListener("change", () => {
+    if (!pattern.test(input.value)) {
+      input.classList.add("error");
+    } else {
+      input.removeAttribute("class");
+    }
+  });
 }
